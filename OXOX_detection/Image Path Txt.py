e2b61f0a267d3d961fa8378e838efd50b594b45b
TwEggs <q0975040879@gmail.com>
2021-01-17 15:48:52 +0800

    Teaching_v5

---
+++ OXOX_detection/Image Path Txt.py
@@ -0,0 +1,26 @@
+import os
+
+def writetxt(f, txtname, path):
+    WriteFile = open(os.path.join('cfg', txtname), 'a+', encoding="utf-8")
+    WriteFile.write(os.getcwd() + '\\' + path + '\\' + f)
+    WriteFile.write('\n')
+    WriteFile.close()
+
+def Allfile(path, files, txtname):
+    for f in files:
+        fullpath = os.path.join(path, f)
+        if os.path.isfile(fullpath):
+            writetxt(f, txtname, path)
+        elif os.path.isdir(fullpath):
+            pass
+
+trainpath = "yolo_train"
+testpath = "yolo_test"
+train = "train.txt"
+test = "test.txt"
+
+trainfiles = os.listdir(trainpath)
+testfiles = os.listdir(testpath)
+
+Allfile(trainpath, trainfiles, train)
+Allfile(testpath, testfiles, test)
